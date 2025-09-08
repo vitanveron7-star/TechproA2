a62b66da0b80d2ee5a3691bdbf3ceef9c4113a94
vitanveron7-star <vitanveron7@gmail.com>
2025-09-08 05:38:11 +0000

    Done adding output

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
 public class Main {
-		public static void main(String[] args) {
-				System.out.println("SEATWORKELEVEN2025");
-						System.out.println();
-								int [] [] myNumbers = { {1, 4, 2}, {3, 6, 8, 5, 2} };
-										
-												for (int row =0; row < myNumbers.length;row++) {
-															for (int col =0; col <
-																		myNumbers [row].length; col++) {
-																						System.out.println("myNumbers[" + row + "][" + col + "] = " + myNumbers [row] [col]);
-																									}
-																											}
-																												}
-																												}
+	    public static void main(String[] args) {
+		        System.out.println("SEATWORKELEVENPART2");
+				        int[][] myNumbers = { {1, 4, 2}, {3, 6, 9} };
+						        
+								        System.out.println(myNumbers[1][2]);
+										    }
+											}
