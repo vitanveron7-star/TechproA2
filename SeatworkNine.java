e6e9d5b8e55123460ed88c92b7ca72fe664e6972
vitanveron7-star <vitanveron7@gmail.com>
2025-09-08 04:52:53 +0000

    Done adding output

---
+++ SeatworkNine.java
@@ -0,0 +1,13 @@
+public class SeatworkNine {
+	    public static void main(String args[]) {
+		        int[] numbers = {10, 20, 30, 40, 50};
+				        int index = 0;
+
+						        while (index < 5) {
+								            System.out.print("Value of item : " + numbers[index]);
+											            index++;
+														            System.out.print("\n");
+																	        
+																			    }
+																				}
+}
