39fa66f402115d088590e1885922993f633ef5bb
vitanveron7-star <vitanveron7@gmail.com>
2025-09-08 05:26:51 +0000

    Done adding output

--- SeatworkTen.java
+++
@@ -1,9 +0,0 @@
-public class SeatworkTen {
-	    public static void main(String args[]) {
-		        for (int x = 10; x < 20; x = x + 1) {
-				            System.out.print("Value of x : " + x);
-							            System.out.print("\n");
-										        
-												    }
-													}
-}
